6418df53a0f216547bde472f5521a20be091b7e2
samarskiyad <samarskiyad@student.bmstu.ru>
2025-01-09 18:46:21 +0300

    Создание приложений about и contact

---
+++ event_manager/contact/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        return HttpResponse("Спасибо за ваше сообщение!")
+    return render(request, 'contact/contact.html')
