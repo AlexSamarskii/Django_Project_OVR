1e0f757f48c79a81711c4ba77f4e8835738ef9bd
samarskiyad <samarskiyad@student.bmstu.ru>
2025-01-12 00:50:07 +0300

    Создание моделей и миграций в Django

--- event_manager/events/admin.py
+++ event_manager/events/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Category, Event, Registration, Review, Comment, Profile, Venue, Sponsor
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ('name',)
+    
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date', 'category', 'venue')
+    list_filter = ('category', 'date')
+    search_fields = ('title', 'description')
+    date_hierarchy = 'date'
+    
+@admin.register(Venue)
+class VenueAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'capacity')
+    search_fields = ('name', 'address')
+    
+admin.site.register(Registration)
+admin.site.register(Review)
+admin.site.register(Comment)
+admin.site.register(Profile)
+admin.site.register(Sponsor)
